113cf939127716629c371607434d71b9d706bb63
Adrien Leblanc <adrienleblanc53@gmail.com>
2020-04-09 14:18:16 +0200

    Ajout du City controller

---
+++ back/src/main/java/com/cinecity/controller/CityController.java
@@ -0,0 +1,48 @@
+package com.cinecity.controller;
+
+import com.cinecity.entities.City;
+import com.cinecity.exception.ResourceNotFoundException;
+import com.cinecity.repository.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class CityController {
+
+    @Autowired
+    private CityRepository cityRepository;
+
+    @GetMapping("/citys")
+    public List<City> getAllCitys() {
+        return cityRepository.findAll();
+    }
+
+    @PostMapping("/citys")
+    public City createCity(@Valid @RequestBody City city) {
+        return cityRepository.save(city);
+    }
+
+    @PutMapping("/citys/{cityId}")
+    public City updateCity(@PathVariable Long cityId, @Valid @RequestBody City cityRequest) {
+        return cityRepository.findById(cityId).map(city -> {
+            city.setId(cityRequest.getId());
+            city.setName(cityRequest.getName());
+            city.setPostalCode(cityRequest.getPostalCode());
+            city.setCountry(cityRequest.getCountry());
+            return cityRepository.save(city);
+        }).orElseThrow(() -> new ResourceNotFoundException("CityId " + cityId + " not found"));
+    }
+
+    @DeleteMapping("/citys/{cityId}")
+    public ResponseEntity<?> deleteCity(@PathVariable Long cityId) {
+        return cityRepository.findById(cityId).map(city -> {
+            cityRepository.delete(city);
+            return ResponseEntity.ok().build();
+        }).orElseThrow(() -> new ResourceNotFoundException("CityId " + cityId + " not found"));
+    }
+}
