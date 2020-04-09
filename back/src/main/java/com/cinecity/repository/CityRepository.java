d3a11b4ac76179e3396f6c98163e4d0e697f8629
Adrien Leblanc <adrienleblanc53@gmail.com>
2020-04-09 15:39:08 +0200

    Fix imports CityRepository

--- back/src/main/java/com/cinecity/repository/CityRepository.java
+++ back/src/main/java/com/cinecity/repository/CityRepository.java
@@ -1,7 +1,6 @@
 package com.cinecity.repository;
 
 import com.cinecity.entities.City;
-import com.cinecity.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CityRepository extends JpaRepository<City, Long> {
