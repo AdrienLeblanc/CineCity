64760bf0d9cb6fb2f78d80d52a142da7a5cab5bd
Adrien Leblanc <adrienleblanc53@gmail.com>
2020-04-09 12:21:08 +0200

    Ajout entity City

---
+++ back/src/main/java/com/cinecity/entities/City.java
@@ -0,0 +1,87 @@
+package com.cinecity.entities;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "cities")
+public class City {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "firstname")
+    private String name;
+
+    @Column(name = "lastname")
+    private String postal_code;
+
+    @Column(name = "type")
+    private String country;
+
+    public City(long id, String name, String postal_code, String country) {
+        this.id = id;
+        this.name = name;
+        this.postal_code = postal_code;
+        this.country = country;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPostal_code() {
+        return postal_code;
+    }
+
+    public void setPostal_code(String postal_code) {
+        this.postal_code = postal_code;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", postal_code='" + postal_code + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof City)) return false;
+        City city = (City) o;
+        return Objects.equals(getName(), city.getName()) &&
+                Objects.equals(getPostal_code(), city.getPostal_code()) &&
+                Objects.equals(getCountry(), city.getCountry());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getPostal_code(), getCountry());
+    }
+}
