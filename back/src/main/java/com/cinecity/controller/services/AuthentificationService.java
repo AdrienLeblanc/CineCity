8586669d14dd1a2c9953482c456db478a3852150
Adrien Leblanc <adrienleblanc53@gmail.com>
2020-04-14 01:07:45 +0200

    Ajout du service d'Authentification (simple)

---
+++ back/src/main/java/com/cinecity/controller/services/AuthentificationService.java
@@ -0,0 +1,29 @@
+package com.cinecity.controller.services;
+
+import com.cinecity.entities.User;
+import com.cinecity.entities.dto.Login;
+import com.cinecity.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.Optional;
+
+@RestController
+public class AuthentificationService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PutMapping("/auth")
+    public Boolean authentification(@Valid @RequestBody Login loginRequest) {
+        Optional<User> queryResult = userRepository.findByLogin(loginRequest.getLogin());
+        if (queryResult.isPresent()) {
+            User user = queryResult.get();
+            return user.getPassword().equals(loginRequest.getPassword());
+        }
+        return Boolean.FALSE;
+    }
+}
